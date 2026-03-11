9c7807839aa1d13521f30ba581c6192f038c7e2a
Guillermo <70928467+Mandamientos@users.noreply.github.com>
2026-03-10 20:18:03 -0600

    + Create Account
    TODO: Hacer las validaciones correspondientes para cada tipo de dato. (El input del ACC_NUM no debe recibir letras ni el de BALANCE)

--- struct_acc.asm
+++ struct_acc.asm
@@ -1,4 +1,3 @@
-
 .MODEL SMALL
 .STACK 100h
 
@@ -9,45 +8,81 @@
     ; ACC_HOLDER 20 bytes
     ; ACC_BAL 4 bytes
     ; ACC_STATE 1 byte
-    
-    ;---Opciones del menu ---
-    
-    op_Crear db "1. Crear cuenta"
-    op_Depos db "2. Depositar Dinero"
-    op_Retir db "3. Retirar Dinero"
-    op_Consul db "4. Consultar Saldo"
-    op_MostRepo db "5. Mostrar Reporte General"
-    op_Desac db "6. Desactivar cuenta"
-    op_Salir db "7. Salir"
-                             
-    ;------------------------                             
-    
-    
     MAX_ACC     equ 10
-    ACC_SIZE    equ 27
+    ACC_SIZE    equ 28
     
     ACC_NUM     equ 0
     ACC_HOLDER  equ 2
-    ACC_BAL     equ 22
-    ACC_STATE   equ 26
+    ACC_BAL     equ 23
+    ACC_STATE   equ 27
     
     ; Variables para el manejo de cuentas
     ACCOUNTS    db ACC_SIZE * MAX_ACC dup(?) ; Define el espacio de memoria para diez cuentas.
-    ACC_COUNT   db 0 ; Contador de cuentas
+    ACC_COUNT   db 0 ; Contador de cuentas 
+                   
+    CURRENT_ACC dw ? ; Cuenta seleccionada
     
-    HOLDER_TEST_INPUT  db 21, ?, 21 dup(?)
-    ACC_NUM_TEST_INPUT db 6, ?, 6 dup(?)
-    
+    HOLDER_INPUT        db 21, ?, 21 dup(?)
+    ACC_NUM_INPUT       db 6, ?, 6 dup(?)
+    NUMBER32_INPUT      db 11, ?, 11 dup(?)
     
 .CODE
 
-print_menu PROC
-    mov ds, op_Crear
-    mov ah, 09h
-    lea dx, op_Crear
-    int 21h
+; En number32_input debe de estar el numero
+; Recibe AX, BX, CX y DX en 0   alta:baja 
+; Devuelve en AX, DX un numero    dx:ax
+; de 32 bits.                     bx:cx
+
+convert_32 PROC
+    mov ax, 0 ; Limpiar cuatro registros para convertir un numero de 32 bits
+    mov bx, 0 
+    mov cx, 0
+    mov dx, 0
     
+    xor di, di
+    mov dl, [NUMBER32_INPUT+1]
+    mov di, dx
+    xor dx, dx
+    lea si, NUMBER32_INPUT+2
+
+convert_loop:
+    cmp di, 0
+    je exit
+                    
+    mov bl, [si]
+    sub bl, '0'
     
+    push bx
+    
+    shl ax, 1   
+    rcl dx, 1
+    
+    mov bx, ax
+    mov cx, dx
+    
+    shl ax, 1
+    rcl dx, 1
+    
+    shl ax, 1
+    rcl dx, 1
+    
+    add ax, bx
+    adc dx, cx
+    
+    pop bx
+    
+    xor bh, bh
+    add ax, bx
+    adc dx, 0
+    
+    dec di
+    inc si
+    jmp convert_loop  
+    
+exit:
+    ret
+    
+convert_32 ENDP
 
 create_account PROC
     mov al, MAX_ACC
@@ -55,19 +90,21 @@ create_account PROC
     jae acc_limit ; Se fija si hay espacio para una nueva cuenta
     
     mov ah, 0Ah
-    lea dx, HOLDER_TEST_INPUT ; INPUT DE TESTEO
+    lea dx, HOLDER_INPUT ; INPUT DE TESTEO
     int 21h
     
-    mov ah, 0Ah
-    lea dx, ACC_NUM_TEST_INPUT ; INPUT DE TESTEO
+    lea dx, ACC_NUM_INPUT ; INPUT DE TESTEO
     int 21h
+    
+    lea dx, NUMBER32_INPUT
+    int 21h 
     
     xor ax, ax ; Limpiar registros
     xor cx, cx
     xor dx, dx
     xor bh, bh
-    mov cl, [ACC_NUM_TEST_INPUT+1] ; Numero de caracteres que el usuario ingreso
-    lea si, ACC_NUM_TEST_INPUT+2   ; La direccion de memoria donde empieza el string
+    mov cl, [ACC_NUM_INPUT+1] ; Numero de caracteres que el usuario ingreso
+    lea si, ACC_NUM_INPUT+2   ; La direccion de memoria donde empieza el string
 
 convert_acc_number:
     mov bl, [si]  ; Asignamos el caracter
@@ -98,16 +135,45 @@ check_existance:
 
 create_new_acc:
     mov dx, ax
-    xor ax, ax
-    mov al, ACC_COUNT
-    mov bl, ACC_SIZE
+    xor ax, ax               ; Estas lineas se utilizan para calcular
+    mov al, ACC_COUNT        ; la direccion de memoria de la cuenta que vamos a crear
+    mov bl, ACC_SIZE         ; de la siguiente forma: ACCOUNTS + ACC_SIZE * ACC_COUNT
     mul bl
     lea si, ACCOUNTS
     add si, ax
+    mov CURRENT_ACC, si
     
-    mov [si+ACC_NUM], dx
+    mov [si+ACC_NUM], dx     ; Guarda el numero de cuenta
+    mov [si+ACC_STATE], 1    ; Colocamos el estado de la cuenta en activa (0 = inactiva, 1 = activa)
+    lea di, HOLDER_INPUT     ; Primer byte del de input
+    mov cl, [di+1]           ; Contador de caracteres, para un el loop de copy_acc_holder
+    lea di, HOLDER_INPUT+2   ; Primer caracter del input
+    lea si, si+ACC_HOLDER    ; Primer caracter del arreglo de la cuenta
+    mov bx, ax
+    
+copy_acc_holder:
+    mov dx, 0   
+    
+    mov al, [di]          ; Copiar del input a la cuenta que estamos creando
+    mov [si], al
+    
+    inc di                ; Cambiamos de byte (caracter) para el input y el arreglo de la cuenta      
+    inc si                       
+    
+    loop copy_acc_holder         
+    
+    mov al, '$'                  ; Al final de la cadena del nombre se le coloca $ para poder imprimir
+    mov [si], al                 ; sin usar un loop
+
+    call convert_32
+    
+    mov si, CURRENT_ACC
+    
+    mov [si+ACC_BAL], ax
+    mov [si+ACC_BAL+2], dx
     
     inc ACC_COUNT
+    ret    
      
 acc_limit:
     ret
@@ -126,11 +192,8 @@ main:
     mov ax, @data
     mov ds, ax
     
-    call print_menu    
     call create_account
     call create_account 
     
     call end_program
-END main 
-
-
+END main
